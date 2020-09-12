f69b132ade7a14199b217416627db42529714641
khermalik <khermalik@gmail.com>
2020-09-12 09:30:22 -0700

    Commit 4 - Fizz Buzz answered

--- scripts/app.js
+++ scripts/app.js
@@ -15,11 +15,19 @@
 // for (i = 0; i < 20; i++) {
 //   if (i % 2 === 0) {
 //     const catTalk = Math.floor(Math.random() * 3);
-//     if (catTalk === 0) console.log('Human...why you taking pictures of me?')
+//     if (catTalk === 0) console.log('Human...why you taking pictures of me?');
 //     else if (catTalk === 1) console.log('The catnip made me do it.');
-//     else console.log('Why does the red dot always get away?')
+//     else console.log('Why does the red dot always get away?');
 //   } else {
 //       console.log('Love me, pet me! HSSSSSSS');
 //   }
 // }
 
+// ------------------ Fizz Buzz ---------------- //
+
+// for (i = 1; i <= 100; i++) {
+//   if (i % 15 === 0) console.log('FizzBuzz');
+//   else if (i % 3 === 0) console.log('Fizz');
+//   else if (i % 5 === 0) console.log('Buzz');
+//   else console.log(i);
+// }
