23122728ac05e96c49764ae6d59f975b2b723f38
khermalik <khermalik@gmail.com>
2020-09-12 09:11:20 -0700

    Commit 3 - Excited Kittens answered

--- scripts/app.js
+++ scripts/app.js
@@ -9,3 +9,17 @@
 // for (i = 0; i <= 200; i += 2) {
 //   console.log(i);
 // }
+
+// --------------- Excited Kitten -------------- //
+
+// for (i = 0; i < 20; i++) {
+//   if (i % 2 === 0) {
+//     const catTalk = Math.floor(Math.random() * 3);
+//     if (catTalk === 0) console.log('Human...why you taking pictures of me?')
+//     else if (catTalk === 1) console.log('The catnip made me do it.');
+//     else console.log('Why does the red dot always get away?')
+//   } else {
+//       console.log('Love me, pet me! HSSSSSSS');
+//   }
+// }
+
