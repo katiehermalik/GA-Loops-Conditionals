b5095d573a83fc4155e193df23402208100c8eff
khermalik <khermalik@gmail.com>
2020-09-12 10:05:59 -0700

    Commit 5 - Getting to Know You answered

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,16 @@
-// -------------- Easy Going -------------- //
+// //-------------- Easy Going -------------- //
 
 // for (i = 1; i <= 20; i++) {
 //   console.log(i);
 // }
 
-// --------------- Get Even -------------- //
+// //--------------- Get Even -------------- //
 
 // for (i = 0; i <= 200; i += 2) {
 //   console.log(i);
 // }
 
-// --------------- Excited Kitten -------------- //
+// //--------------- Excited Kitten -------------- //
 
 // for (i = 0; i < 20; i++) {
 //   if (i % 2 === 0) {
@@ -23,7 +23,7 @@
 //   }
 // }
 
-// ------------------ Fizz Buzz ---------------- //
+// //------------------ Fizz Buzz ---------------- //
 
 // for (i = 1; i <= 100; i++) {
 //   if (i % 15 === 0) console.log('FizzBuzz');
@@ -31,3 +31,46 @@
 //   else if (i % 5 === 0) console.log('Buzz');
 //   else console.log(i);
 // }
+
+// //------------- Getting to Know You ------------ //
+
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
+
+// // 1
+
+// kenny.splice(0, 1, "Gameboy");
+// console.log(kenny);
+
+// // 2
+
+// jimClark.splice(1, 1, (jimClark[1]+1));
+// console.log(jimClark);
+
+// // 3
+
+// ryan.splice(2, 1, "Gotham City");
+// console.log(ryan);
+
+// // 4
+
+// reuben.pop();
+// console.log(reuben);
+// reuben.push("Chicago");
+// console.log(reuben);
+
+// // 5
+
+// jimHaff.pop();
+// console.log(jimHaff);
+// jimHaff.push("Chicago", "Bisbee", "Los Angeles");
+// console.log(jimHaff);
+
+// // 6
+
+// jimHaff.splice(2, 1);
+// console.log(jimHaff);
+
